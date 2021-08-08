64ae536ed213161f84babb7f03b791581f5c0a07
samedamci <samedamci@disroot.org>
2021-08-08 12:17:12 +0200

    Add custom_help_command decorator to Telegrask class

--- telegrask/helpparser.py
+++ telegrask/helpparser.py
@@ -4,13 +4,13 @@ class HelpParser:
     def __init__(self, inline: bool = False) -> None:
         self.__message = "*Available commands*\n\n"
         self.__final_message = self.__message
-        self.__commands_descriptions = dict()
+        self.commands_descriptions = dict()
 
     def add_command(self, name: str, help: str) -> None:
-        self.__commands_descriptions[name] = help
+        self.commands_descriptions[name] = help
 
     @property
     def content(self) -> str:
-        for cmd, desc in self.__commands_descriptions.items():
+        for cmd, desc in self.commands_descriptions.items():
             self.__final_message += f"/{cmd}\n{desc}\n\n"
         return self.__final_message
