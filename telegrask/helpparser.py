98c76801045de5a5da02f7a389a449405e5252f8
samedamci <samedamci@disroot.org>
2021-08-19 20:59:03 +0200

    Fix bug with doubled commands in HelpParser class

--- telegrask/helpparser.py
+++ telegrask/helpparser.py
@@ -3,7 +3,6 @@ class HelpParser:
 
     def __init__(self, inline: bool = False) -> None:
         self.__message = "*Available commands*\n\n"
-        self.__final_message = self.__message
         self.commands_descriptions = dict()
 
     def add_command(self, name: str, help: str) -> None:
@@ -11,6 +10,7 @@ class HelpParser:
 
     @property
     def content(self) -> str:
+        final_message = self.__message
         for cmd, desc in self.commands_descriptions.items():
-            self.__final_message += f"/{cmd}\n{desc}\n\n"
-        return self.__final_message
+            final_message += f"/{cmd}\n{desc}\n\n"
+        return final_message
