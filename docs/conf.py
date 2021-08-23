ba78977b0f437e5d740f8e4dc802b2c4c8e38550
samedamci <samedamci@disroot.org>
2021-08-23 22:20:26 +0200

    Update project version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, samedamci'
 author = 'samedamci'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.4'
+release = '0.3.6'
 
 
 # -- General configuration ---------------------------------------------------
