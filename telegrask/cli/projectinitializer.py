86be9d686dd2988eb6760daafb45724b8f008b3b
samedamci <samedamci@disroot.org>
2021-08-24 15:00:36 +0200

    Change venv directory in ProjectInitializer and use custom prompt

--- telegrask/cli/projectinitializer.py
+++ telegrask/cli/projectinitializer.py
@@ -10,18 +10,19 @@ class ProjectInitializer:
         create_venv: bool = True,
         include_gitignore: bool = True,
     ) -> None:
+        self.project_name = project_name
         if create_venv:
             self.create_venv()
         if include_gitignore:
             self.add_gitignore()
-        self.create_package(project_name)
+        self.create_package()
 
-    @staticmethod
-    def create_venv() -> None:
+    def create_venv(self) -> None:
         venv.create(
-            path.join(ctx.app_root, ctx.venv_dir, "venv"),
+            path.join(ctx.app_root, ctx.venv_dir),
             system_site_packages=True,
             with_pip=True,
+            prompt=self.project_name,
         )
 
     @staticmethod
@@ -41,9 +42,9 @@ class ProjectInitializer:
             template.replace("__VENV_DIR", ctx.venv_dir),
         )
 
-    def create_package(self, package_name: str) -> None:
-        mkdir(path.join(ctx.app_root, package_name))
-        package_dir = path.join(ctx.app_root, package_name)
+    def create_package(self) -> None:
+        mkdir(path.join(ctx.app_root, self.project_name))
+        package_dir = path.join(ctx.app_root, self.project_name)
         for template in ["__init__.py", "config.py", "commands.py"]:
             self.write_file(
                 path.join(package_dir, template), self.read_template(template)
